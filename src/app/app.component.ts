cd8c2c6a2cb79c06921acbdc3e961460a9fc5fd4
Erlan-Bam <erlanzh.gg@gmail.com>
2024-11-18 20:42:10 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'samigroup';
+  logos = [
+    {
+      src: '/assets/agro_logo.svg',
+      alt: 'Agro Logo',
+      link: 'https://agro.example.com',
+      text: 'Перейти на сайт Agro',
+    },
+    {
+      src: '/assets/spark_logo.svg',
+      alt: 'Spark Logo',
+      link: 'https://spark.example.com',
+      text: 'Перейти на сайт Spark',
+    },
+    {
+      src: '/assets/sun_logo.svg',
+      alt: 'Sun Logo',
+      link: 'https://sun.example.com',
+      text: 'Перейти на сайт Sun',
+    },
+  ];
 }
